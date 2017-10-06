946dc5074741f60fc671ea4f14a16adf0ad475ce
marlontamo <marlontamo@gmail.com>
2017-10-06 17:12:35 +0800

    adding button for showing the store peoducts

--- application/views/user/store/storeProfile.php
+++ application/views/user/store/storeProfile.php
@@ -1,6 +1,8 @@
 <?php $this->load->view('header'); ?>
 <h1> Store Profile</h1>
 <div class="row">
+<?php echo "<h1>".$_SESSION['user_id']."</h1>"; ?>
+
  <table class="table-hover table">
  	<thead>
  		<th>Store Id</th>
@@ -18,7 +20,7 @@
            echo "<td>".$key->store_name."</td>";
             echo "<td>".$key->address_id."</td>";
              echo "<td>".$this->store->get_user_by_id($key->user_id)."</td>";
-             echo "<td><a href='".base_url('store/edit')."/".$key->store_id."' class='btn-danger'>edit<a><a href='".base_url('store/view')."/".$key->store_id."' class='btn-warning'>view<a><a href='' class='btn-primary'>delete<a></td>";
+             echo "<td><a href='".base_url('store/edit')."/".$key->store_id."' class='btn-danger'>edit<a><a href='".base_url('store/view')."/".$key->store_id."' class='btn-warning'>view<a><a href='' class='btn-primary'>delete</a><a href='' class='btn btn-xs btn-danger'>show my products</a></td>";
           }
           ?>
  		
